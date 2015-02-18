79eb0205787d31701f6b7d45022f2b022e5efde9
4nn3ck <4nn3ck@gmail.com>
2015-02-18 08:22:06 +0100

    WiP for sami

---
+++ .sami/config.php
@@ -0,0 +1,30 @@
+<?php
+
+use Sami\Sami;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('Resources')
+    ->exclude('Tests')
+    ->in($dir = __DIR__.'/../src')
+;
+
+$versions = GitVersionCollection::create($dir)
+    ->addFromTags('0.0.*')
+    ->add('dev', '0.0.1-dev')
+    ->add('master', 'master')
+;
+
+return new Sami($iterator, array(
+    'theme'                => 'symfony',
+    'versions'             => $versions,
+    'title'                => '4373Alpha-Server API',
+    'build_dir'            => __DIR__.'/../build/sf2/%version%',
+    'cache_dir'            => __DIR__.'/../cache/sf2/%version%',
+    // use a custom theme directory
+    // 'template_dirs'        => array(__DIR__.'/themes/symfony'),
+    'default_opened_level' => 2,
+));
