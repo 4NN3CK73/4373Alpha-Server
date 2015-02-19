c2146a524496fe808cedf4d2e82dc963a8127ca4
4nn3ck <4nn3ck@gmail.com>
2015-02-19 14:52:57 +0100

    GameCore WiP The First test case yeah!

---
+++ tests/Anneck/Game/GameProductFactoryTest.php
@@ -0,0 +1,36 @@
+<?php
+/************************************************************************
+ * This file is part of 4373Alpha-Server Project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * ************************************************************************
+ * Created at 19.02.15, 12:43 by 4nn3ck
+ * ************************************************************************
+ */
+
+namespace Anneck\Game;
+
+
+use Anneck\Game\Exception\GameException;
+use Anneck\Game\Worlds\GameWorld;
+use PHPUnit_Framework_TestCase;
+
+class GameProductFactoryTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testCreationOfGameProductFactory()
+    {
+        $gameProductFactory = null; // Default to enable test fail with null.
+        
+        try {
+            $gameProductFactory = GameProductFactory::getInstance(new GameWorld());
+        } catch (GameException $gameException) {
+            static::fail($gameException->getMessage());
+        }
+
+
+        static::assertNotNull($gameProductFactory, 'Failed to create the GameProductFactory');
+    }
+
+}
