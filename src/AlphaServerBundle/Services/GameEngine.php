e0cc3cd4c8551771c77dcc46e58b50d3f0f8a9f4
4nn3ck <4nn3ck@gmail.com>
2015-03-03 14:30:07 +0100

    GameCore: just checkstyle fixes

--- src/AlphaServerBundle/Services/GameEngine.php
+++ src/AlphaServerBundle/Services/GameEngine.php
@@ -5,17 +5,16 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  * ************************************************************************
- * Created at 03.03.15, 13:36 by 4nn3ck  
+ * Created at 03.03.15, 13:36 by 4nn3ck
  * ************************************************************************
  */
 
 namespace AlphaServerBundle\Services;
 
-
 use Monolog\Logger;
 
-class GameEngine {
-
+class GameEngine
+{
     private $mailer, $logger;
 
     public function __construct(\Swift_Mailer $mailer, Logger $logger)
@@ -27,8 +26,7 @@ class GameEngine {
     public function nextTurn()
     {
         $this->logger->addNotice('Called nextTurn()');
+
         return 'GameEngine';
-
     }
-
 }
